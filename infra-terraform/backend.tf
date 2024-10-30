0151cc6a183920da4a96887f5073df3f3c55c77d
George Akanza <gakanza@gmail.com>
2024-10-30 14:28:25 -0400

    updated

--- infra-terraform/backend.tf
+++ infra-terraform/backend.tf
@@ -1,13 +1,11 @@
 terraform {
   backend "s3" {
     bucket = "ec2-builder-remote-state"
-    key    = "ec2-image-builder/us-east-2"
+    key    = "ec2-image-builder/state.tfstate"
     region = "us-east-2"
 
     # Replace this with your DynamoDB table name!
     dynamodb_table = "anslem-terraform-tf-state-lock"
   }
 }
-
-
-##rterrgr
+ 
