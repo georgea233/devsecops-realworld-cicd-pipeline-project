82f6304f7393c87b5c2ce2ea93f44d67be8eb990
georgea233 <gakanza@gmail.com>
2025-04-09 19:51:37 -0400

    source code

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "ec2-builder-remote-state"
-    key    = "ec2-image-builder/us-east-2"
+    bucket = "fluentindevops-tf-statefile"
+    key    = "fluentindevops-tf-statefile/eks-infra/terraform.tfstate"
     region = "us-east-2"
 
     # Replace this with your DynamoDB table name!
-    dynamodb_table = "anslem-terraform-tf-state-lock"
+    dynamodb_table = "fluentindevops-tf-state-lock"
   }
 }
