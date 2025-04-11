87d4bcd8b17742a24cadf47bbe0572658ba54fd1
georgea233 <gakanza@gmail.com>
2025-04-11 18:44:05 -0400

    destroy

--- terraform/eks/eks_module/iam.tf
+++ terraform/eks/eks_module/iam.tf
@@ -39,7 +39,7 @@ resource "aws_iam_policy" "eks_cluster_policy" {
     ]
   })
 }
-#degfrdfdf
+
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = aws_iam_policy.eks_cluster_policy.arn
